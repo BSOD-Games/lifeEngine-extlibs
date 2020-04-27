cfae066af2ab5d8250f5040ba442bcc36f26e948
zombihello <zombihello@yandex.ru>
2020-04-27 16:56:08 +0300

    Добавил скрипт установки бинарников в директорию билда движка

---
+++ cmake/install_extlibs.cmake
@@ -0,0 +1,14 @@
+function( install_extlibs EXTLIBS_DIR INSTALL_DIR )
+	if( ${CMAKE_SYSTEM_NAME} MATCHES "Windows" )
+		set( BINARES
+				"${EXTLIBS_DIR}/FreeImage/bin/FreeImage.dll"
+				"${EXTLIBS_DIR}/freetype/bin/freetype.dll"
+				"${EXTLIBS_DIR}/GLEW/bin/glew32.dll"
+				"${EXTLIBS_DIR}/SDL2/bin/SDL2.dll"
+				"${EXTLIBS_DIR}/tcc/bin/libtcc.dll" )
+	else()
+		message( SEND_ERROR "Unknow platform" )
+	endif()	
+	
+	install( FILES ${BINARES} DESTINATION ${INSTALL_DIR} )
+endfunction()
