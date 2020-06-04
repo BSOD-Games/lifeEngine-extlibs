f0b1ebdf888bd5995892ea3e8d7982c0353cbb93
ZhulaiIvan <zhulaiivan.dnipro@gmail.com>
2020-06-04 16:52:34 +0300

    Исправлен баг с загрузкой аудио

--- cmake/install_extlibs.cmake
+++ cmake/install_extlibs.cmake
@@ -5,7 +5,8 @@ function( install_extlibs EXTLIBS_DIR INSTALL_DIR )
 				"${EXTLIBS_DIR}/freetype/bin/freetype.dll"
 				"${EXTLIBS_DIR}/GLEW/bin/glew32.dll"
 				"${EXTLIBS_DIR}/SDL2/bin/SDL2.dll"
-				"${EXTLIBS_DIR}/tcc/bin/libtcc.dll" )
+				"${EXTLIBS_DIR}/tcc/bin/libtcc.dll"
+				"${EXTLIBS_DIR}/OpenAL/bin/OpenAl32.dll" )
 	else()
 		message( SEND_ERROR "Unknow platform" )
 	endif()	
