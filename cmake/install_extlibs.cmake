8fa78e3b272eddd3ef778c1c2026ff500a31daf5
zombihello <zombihello@yandex.ru>
2020-10-17 23:47:59 +0300

    Добавил скрипт установки зависимостей

--- cmake/install_extlibs.cmake
+++ cmake/install_extlibs.cmake
@@ -1,17 +1,11 @@
 function( install_extlibs EXTLIBS_DIR INSTALL_DIR )
-	if( ${CMAKE_SYSTEM_NAME} MATCHES "Windows" )
-		set( BINARES
-				"${EXTLIBS_DIR}/FreeImage/bin/FreeImage.dll"
-				"${EXTLIBS_DIR}/freetype/bin/freetype.dll"
-				"${EXTLIBS_DIR}/GLEW/bin/glew32.dll"
-				"${EXTLIBS_DIR}/SDL2/bin/SDL2.dll"
-				"${EXTLIBS_DIR}/OpenAL/bin/OpenAL32.dll"
-				"${EXTLIBS_DIR}/steamaudio_api/bin/Windows/x86/phonon.dll"
-				"${EXTLIBS_DIR}/luajit/bin/lua51.dll"
-				)
-	else()
-		message( SEND_ERROR "Unknow platform" )
-	endif()	
-	
-	install( FILES ${BINARES} DESTINATION ${INSTALL_DIR} )
+	set( CMAKE_FILES 
+			"${EXTLIBS_DIR}/cmake/FindGLEW.cmake"
+			"${EXTLIBS_DIR}/cmake/FindSDL2.cmake"
+			"${EXTLIBS_DIR}/cmake/FindTMXLite.cmake" )
+
+	install( FILES ${CMAKE_FILES} DESTINATION ${INSTALL_DIR}/CMake )
+	install( DIRECTORY ${EXTLIBS_DIR}/GLEW DESTINATION ${INSTALL_DIR} )
+	install( DIRECTORY ${EXTLIBS_DIR}/SDL2 DESTINATION ${INSTALL_DIR} )
+	install( DIRECTORY ${EXTLIBS_DIR}/TMXLite DESTINATION ${INSTALL_DIR} )
 endfunction()
